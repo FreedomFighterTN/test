cfafd1ef49107d68b5b4c7882ea210457908ed8f
FreedomFighterTN <45715897+FreedomFighterTN@users.noreply.github.com>
2020-01-14 08:46:45 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("666546612125827072")
+setInterval(function() {
+channel.send(`3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime 3abch9ime `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
